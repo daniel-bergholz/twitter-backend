d9c5f7762f86383512f72d3ce4802003e5ce178c
Daniel Bergholz <daniel@maratonas.academy>
2021-09-11 09:35:54 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import helmet from 'fastify-helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -15,6 +17,27 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.register(helmet);
   app.enableCors({ origin: process.env.CORS_ORIGIN || '*' });
+
+  app.register(helmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Twitter API')
+    .setDescription('Documentação do backend do Twitter')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 
