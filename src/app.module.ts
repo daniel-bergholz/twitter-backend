84f5d7c58d5f2207c41ef341344e72be97ca98a3
Daniel Bergholz <daniel@maratonas.academy>
2021-09-17 14:56:42 -0300

    feat: add rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
 import { TweetsModule } from './modules/tweets/tweets.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule, UsersModule, TweetsModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 10,
+      limit: 6,
+    }),
+    AuthModule,
+    UsersModule,
+    TweetsModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
